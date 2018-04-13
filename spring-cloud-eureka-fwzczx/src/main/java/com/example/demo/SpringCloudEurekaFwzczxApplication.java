6fd09c3b28a92c77976dd70693a0ce434e13f249
zhongenguang <959751561@qq.com>
2018-04-13 20:11:32 +0800

    Initial commit

---
+++ spring-cloud-eureka-fwzczx/src/main/java/com/example/demo/SpringCloudEurekaFwzczxApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer//服务注册中心
+@SpringBootApplication
+public class SpringCloudEurekaFwzczxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudEurekaFwzczxApplication.class, args);
+	}
+}
